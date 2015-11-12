71d90f6a9c9c0f8c98a8d4f26d417651c609df61
Jean-Richard Lai <jrichardlai@gmail.com>
2015-11-12 09:19:15 -0800

    Add Example App

---
+++ Example/index.ios.js
@@ -0,0 +1,96 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+import React from 'react-native';
+const {
+  AppRegistry,
+  StyleSheet,
+  View,
+  LinkingIOS,
+  AlertIOS,
+} = React;
+
+import ParsedText from 'react-native-parsed-text';
+
+class Example extends React.Component {
+  static displayName = 'Example';
+
+  handleUrlPress(url) {
+    LinkingIOS.openURL(url);
+  }
+
+  handlePhonePress(phone) {
+    AlertIOS.alert(`${phone} has been pressed!`);
+  }
+
+  handleNamePress(name) {
+    AlertIOS.alert(`Hello ${name}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ParsedText
+          style={styles.text}
+          parse={
+            [
+              {type: 'url',          style: styles.url, onPress: this.handleUrlPress},
+              {type: 'phone',        style: styles.phone, onPress: this.handlePhonePress},
+              {pattern: /Bob|David/, style: styles.name, onPress: this.handleNamePress},
+              {pattern: /42/,        style: styles.magicNumber},
+              {pattern: /#(\w+)/,    style: styles.hashTag},
+            ]
+          }
+        >
+          Hello this is an example of the ParsedText, links like http://www.google.com or http://www.facebook.com are clickable and phone number 444-555-6666 can call too.
+          But you can also do more with this package, for example Bob will change style and David too.
+          And the magic number is 42!
+          #react #react-native
+        </ParsedText>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  url: {
+    color: 'red',
+    textDecorationLine: 'underline',
+  },
+
+  text: {
+    color: 'black',
+    fontSize: 15,
+  },
+
+  phone: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+
+  name: {
+    color: 'red',
+  },
+
+  magicNumber: {
+    fontSize: 42,
+    color: 'pink',
+  },
+
+  hashTag: {
+    fontStyle: 'italic',
+  },
+
+});
+
+AppRegistry.registerComponent('Example', () => Example);
