7d97c4e4b6015100ed3b08b4f68c8dc75517c30a
Jean-Richard Lai <jrichardlai@gmail.com>
2015-11-17 21:36:30 -0800

    Add email to example

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -30,6 +30,10 @@ class Example extends React.Component {
     AlertIOS.alert(`Hello ${name}`);
   }
 
+  handleEmailPress(email) {
+    AlertIOS.alert(`send email to ${email}`);
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -39,6 +43,7 @@ class Example extends React.Component {
             [
               {type: 'url',          style: styles.url, onPress: this.handleUrlPress},
               {type: 'phone',        style: styles.phone, onPress: this.handlePhonePress},
+              {type: 'email',        style: styles.email, onPress: this.handleEmailPress},
               {pattern: /Bob|David/, style: styles.name, onPress: this.handleNamePress},
               {pattern: /42/,        style: styles.magicNumber},
               {pattern: /#(\w+)/,    style: styles.hashTag},
@@ -46,7 +51,7 @@ class Example extends React.Component {
           }
         >
           Hello this is an example of the ParsedText, links like http://www.google.com or http://www.facebook.com are clickable and phone number 444-555-6666 can call too.
-          But you can also do more with this package, for example Bob will change style and David too.
+          But you can also do more with this package, for example Bob will change style and David too. foo@gmail.com
           And the magic number is 42!
           #react #react-native
         </ParsedText>
@@ -65,6 +70,10 @@ const styles = StyleSheet.create({
 
   url: {
     color: 'red',
+    textDecorationLine: 'underline',
+  },
+
+  email: {
     textDecorationLine: 'underline',
   },
 
