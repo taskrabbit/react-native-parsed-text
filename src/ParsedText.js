a520ed252081ef9a129da2d5d33251e44195415c
Jean-Richard Lai <jrichardlai@gmail.com>
2015-11-17 21:36:21 -0800

    Add email pattern

--- src/ParsedText.js
+++ src/ParsedText.js
@@ -5,6 +5,7 @@ import TextExtraction from './lib/TextExtraction';
 const PATTERNS = {
   url: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/,
   phone: /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/,
+  email: /\S+@\S+\.\S+/,
 };
 
 const defaultParseShape = React.PropTypes.shape({
