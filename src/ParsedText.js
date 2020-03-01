c4d6484bd493ef71435828425f1ab425c21eeb08
AOKI Yuuto <aoki@u-ne.co>
2020-03-02 02:05:39 +0900

    Support long gTLD

--- src/ParsedText.js
+++ src/ParsedText.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import TextExtraction from './lib/TextExtraction';
 
 export const PATTERNS = {
-  url: /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*[-a-zA-Z0-9@:%_\+~#?&\/=])*/i,
+  url: /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.(xn--)?[a-z0-9-]{2,20}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*[-a-zA-Z0-9@:%_\+~#?&\/=])*/i,
   phone: /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,7}/,
   email: /\S+@\S+\.\S+/,
 };
