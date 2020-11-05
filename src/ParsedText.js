467ac608e806609571eee99053d7bc6a3e639d20
Blake <33406511+JongGyuChoi@users.noreply.github.com>
2020-11-05 16:26:35 +0900

    add korean regexp patterns

--- src/ParsedText.js
+++ src/ParsedText.js
@@ -23,7 +23,7 @@ export const PATTERNS = {
    *    - First block is [-a-zA-Z0-9@:%_\+\[\],.~#?&\/=]* -- this matches parameter names & values (including commas, dots, opening & closing brackets)
    *    - The first block must be followed by a closing block [-a-zA-Z0-9@:%_\+\]~#?&\/=] -- this doesn't match commas, dots, and opening brackets
    */
-  url: /(https?:\/\/|www\.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.(xn--)?[a-z0-9-]{2,20}\b([-a-zA-Z0-9@:%_\+\[\],.~#?&\/=]*[-a-zA-Z0-9@:%_\+\]~#?&\/=])*/i,
+  url: /(https?:\/\/|www\.)[-a-zA-Zㄱ-ㅎ가-힣0-9@:%._\+~#=]{1,256}\.(xn--)?[a-zㄱ-ㅎ가-힣0-9-]{2,20}\b([-a-zㄱ-ㅎ가-힣A-Z0-9@:%_\+\[\],.~#?&\/=]*[-a-zㄱ-ㅎ가-힣A-Z0-9@:%_\+\]~#?&\/=])*/i,
   phone: /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,7}/,
   email: /\S+@\S+\.\S+/,
 };
